26c7eff30a8315cbf401faf6fd2fb2d38754a95f
ShivShrivas <shivamsavita2000@gmail.com>
2024-07-29 15:37:30 +0530

    first

--- app/src/main/java/com/org/visus/apis/APIUrl.java
+++ app/src/main/java/com/org/visus/apis/APIUrl.java
@@ -2,5 +2,6 @@ package com.org.visus.apis;
 
 public class APIUrl {
     public static String BASE_URL_UAT = "http://developmentapi.arambhsoftwaresolutions.com/";
-    public static String BASE_URL = "http://visusmiswebapi.arambhsoftwaresolutions.com";
+    public static String BASE_URL = "http://developmentapi.arambhsoftwaresolutions.com/";
+//    public static String BASE_URL = "http://visusmiswebapi.arambhsoftwaresolutions.com";
 }
